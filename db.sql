efc2c25e6a2d2fa2d5037bac54c1b6d263c5e8a1
Rodny Perez <rodnyeperez@gmail.com>
2015-12-08 19:47:51 -0500

    Fix sql statements to match design

--- db.sql
+++ db.sql
@@ -11,15 +11,15 @@ CREATE TABLE categories (
 CREATE TABLE products (
 	upc VARCHAR(12) NOT NULL PRIMARY KEY,
 	name VARCHAR(255),
-	stock_price DOUBLE,
-	on_sale_price DOUBLE,
+	price DOUBLE,
 	category_id INT,
 	date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	thumbnail VARCHAR(255) /* path to the image thumbnail of product */
+	distributor_id INT,
+	manufacturer_id INT
 );
 
 CREATE TABLE supermarkets (
-	id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY, /* The supermarket ID*/
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, /* The supermarket ID*/
 	name VARCHAR(255),
 	address VARCHAR(255),
 	zipcode INT(5),
@@ -33,21 +33,25 @@ CREATE TABLE supermarkets (
 );
 
 CREATE TABLE employees (
-	id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR(255),
 	last_name VARCHAR(255),
 	store_id BIGINT(20),
 	date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	position VARCHAR(255) /* Store manager, cashier, etc..*/
+	position VARCHAR(255), /* Store manager, cashier, etc..*/
+	email_address VARCHAR(150)
 );
 
 CREATE TABLE supermarkets_stock (
-	product_upc INT(12),
-	supermarket_id BIGINT(20),
-	in_stock_count INT(10)
+	product_upc VARCHAR(12),
+	supermarket_id INT,
+	in_stock_count INT(10),
+	has_notification BOOLEAN,
+	date_added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	date_last_updated DATETIME
 );
 
-CREATE TABLE orders (
+/*CREATE TABLE orders (
 	id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	store_id BIGINT(20),
 	net_amount DOUBLE,
@@ -57,9 +61,9 @@ CREATE TABLE orders (
 	employee_id BIGINT(20), /* The id of the employee */	
 	order_type INT(1), /* 0 - New order, 1 - Exchange, 2 - Cancelled order */
 	products_count INT
-);
+);*/
 
-CREATE TABLE products_purchased_in_order (
+/*CREATE TABLE products_purchased_in_order (
 	order_id BIGINT(20),
 	product_up INT(12),
 	number_of_items_purchased INT(5)	
@@ -81,4 +85,4 @@ CREATE TABLE sent_notifications (
 	product_upc INT(12),
 	current_stock INT,
 	manager_id BIGINT(20)
-);
+);*/
