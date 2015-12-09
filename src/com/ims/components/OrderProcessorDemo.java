5b00bab5ddb415f92de04b476e4544c3e4cd7ce9
Rodny Perez <rodnyeperez@gmail.com>
2015-12-09 18:27:20 -0500

    Refactor OrderProcessor demo to use threads

--- src/com/ims/components/OrderProcessorDemo.java
+++ src/com/ims/components/OrderProcessorDemo.java
@@ -2,8 +2,13 @@ package com.ims.components;
 
 public class OrderProcessorDemo {
 	public static void main(String[] args) {
-		Thread receiver = new Thread(new OrderReceiver());
+		OrderProcessor pr = new OrderProcessor();
+		OrderReceiver rv = new OrderReceiver();
+		Thread processor = new Thread(pr);
+		rv.setOrderProcessor(pr);
+		Thread receiver = new Thread(rv);
 		receiver.start();
+		processor.start();
 		System.out.println("I am running");
 	}
 }
