60a9dddc596a1e04f02c32639cf43a8f22b36f22
Gennaro Pesantes <gennaropesantes@gmail.com>
2015-12-10 16:19:33 -0500

    added remove product gui with checkbox thanks to FUCKING RODNY

--- src/com/ims/gui/removeProductFromInventoryView.java
+++ src/com/ims/gui/removeProductFromInventoryView.java
@@ -4,55 +4,105 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class removeProductFromInventoryView extends JFrame {
+public class removeProductFromInventoryView extends JFrame implements ActionListener {
 
 
 
+	JButton returnButton = new JButton("Return");
 	JButton searchButton = new JButton("Search");
-	JButton addButton = new JButton("Add");
+	JButton removeButton = new JButton("Remove");
 	JButton cancelButton = new JButton("Cancel");
 	
-	//JButton returnButton = new JButton("Return to Previous Page");
+
 	JTextField searchTextField = new JTextField(20);
 	JTextArea productArea = new JTextArea();
 	JPanel buttonPanel = new JPanel();
-	JPanel windowPanel = new JPanel();
+
+	JLabel removeLabel = new JLabel("Remove");
+
+
+	String[] columnNames = {"Name",
+            "UPC",
+            "Manufacturer",
+            "Distributor",
+            "Category", "Check"};
+	
+	
+	
+	Object[][] product = {
+		    {"Banana", "24384445485",
+		     "Ecuador", "Manga", "Food", Boolean.FALSE},
+		    {"Apple", "3894745876485",
+		     "USA", "Tupa", "Food", Boolean.FALSE},
+		    {"Cake", "83945745864",
+		     "Bakery", "HP", "Cleaning", Boolean.FALSE},
+
+		};
+	
+	ProductsTableModel model = new ProductsTableModel(product, columnNames);
+	JTable table = new JTable(model);
+	
+	
+	
+		public void removeProduct(){
+		String searchWord = "";
+		searchWord = inventoryManagementSystemView.getSearchField(searchWord);
+		
+	}
 	
 	
 		public removeProductFromInventoryView(){
 			
-		add(windowPanel);
-		windowPanel.add(buttonPanel);
-		
-		windowPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 		setSize(1280, 750);
-		//setLayout(new GridLayout());
+		setLayout(new BorderLayout());
 		setBackground(Color.lightGray);
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		
-		//add(returnButton, BorderLayout.SOUTH);
-		add(buttonPanel, BorderLayout.NORTH);
-		buttonPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		buttonPanel.setLayout(new GridLayout(0,4));
+		
+		add(table, BorderLayout.CENTER);
+		add(searchTextField, BorderLayout.NORTH);
+	
+		
+		add(buttonPanel, BorderLayout.WEST);
+		
+		buttonPanel.setLayout(new GridLayout(0,1));
+		
+		buttonPanel.add(removeLabel);
+		removeLabel.setBorder(BorderFactory.createEmptyBorder(0, 165, 0, 0));
 		
 		buttonPanel.add(searchButton);
-		searchButton.setPreferredSize(new Dimension(200, 150));
-		buttonPanel.add(searchTextField);
-		buttonPanel.add(addButton);
-		addButton.setPreferredSize(new Dimension(200, 150));
-		buttonPanel.add(cancelButton);
-		cancelButton.setPreferredSize(new Dimension(200, 150));
+		searchButton.setPreferredSize(new Dimension(400, 200));
 		
+		buttonPanel.add(removeButton);
+		removeButton.setPreferredSize(new Dimension(400, 200));
+		
+		buttonPanel.add(cancelButton);
+		cancelButton.setPreferredSize(new Dimension(400, 200));
+		
+		buttonPanel.add(returnButton);
+		returnButton.setPreferredSize(new Dimension(400, 200));
+		returnButton.addActionListener(this);
 	
 }
-	
-	
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getActionCommand()==("Return")){
+				super.dispose();
+				
+			}
+		}
 }
