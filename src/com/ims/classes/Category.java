28a826dd5c0c187afc923d218198820ad025df48
Rodny Perez <rodnyeperez@gmail.com>
2015-12-11 16:32:50 -0500

    Add classes to map category, distributors and manf

---
+++ src/com/ims/classes/Category.java
@@ -0,0 +1,32 @@
+package com.ims.classes;
+
+public class Category {
+	private String name;
+	private int id;
+	
+	public Category(int id, String name) {
+		this.name = name;
+		this.id = id;
+	}
+	
+	public Category() {
+		this(0, "No Category");
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String toString() {
+		return "Category id #" + this.id + " name: " + this.name;
+	}
+}
