afd3e99f46fd301a2b534b4cda1401ca4c6bbca9
Rodny Perez <rodnyeperez@gmail.com>
2015-12-11 12:22:18 -0500

    Add missing statements to scheme

--- db.sql
+++ db.sql
@@ -45,7 +45,7 @@ CREATE TABLE employees (
 CREATE TABLE supermarkets_stock (
 	product_upc VARCHAR(12),
 	supermarket_id INT,
-	in_stock_count INT(10),
+	product_count INT(10),
 	has_notification BOOLEAN,
 	date_added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	date_last_updated DATETIME
@@ -58,30 +58,30 @@ CREATE TABLE supermarkets_stock (
 	tax_percent DOUBLE,
 	total_amount DOUBLE,
 	date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	employee_id BIGINT(20), /* The id of the employee */	
-	order_type INT(1), /* 0 - New order, 1 - Exchange, 2 - Cancelled order */
+	employee_id BIGINT(20), The id of the employee 
+	order_type INT(1),  0 - New order, 1 - Exchange, 2 - Cancelled order 
 	products_count INT
-);*/
+);
 
-/*CREATE TABLE products_purchased_in_order (
+CREATE TABLE products_purchased_in_order (
 	order_id BIGINT(20),
 	product_up INT(12),
 	number_of_items_purchased INT(5)	
 );
 
 CREATE TABLE threshold_notifications_list (
-	id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY, /* The id of the notification */
+	id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,  The id of the notification
 	product_upc INT(12),
 	stock_threshold INT,
 	store_id BIGINT(20),
 	manager_id BIGINT(20),
-	frequency INT(1), /* 0 - immediately, 1- hourly, 2- daily */
+	frequency INT(1), 0 - immediately, 1- hourly, 2- daily
 	date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE sent_notifications (
-	id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,  /* The id of this sent notification*/
-	date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, /*date and time last sent */
+	id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,   The id of this sent notification
+	date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, date and time last sent
 	product_upc INT(12),
 	current_stock INT,
 	manager_id BIGINT(20)
