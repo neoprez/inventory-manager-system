1d3b8338d348f99aa3ba7063b9a8e89042d31e48
Rodny Perez <rodnyeperez@gmail.com>
2015-12-12 02:16:40 -0500

    Add logic for inventory tracker to work

--- src/com/ims/components/ProductsTracker.java
+++ src/com/ims/components/ProductsTracker.java
@@ -1,16 +1,85 @@
 package com.ims.components;
 
-public class ProductsTracker {
-	private Notifier 		notifier;
-	private OrderProcessor 	orderProcessor;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+import com.ims.classes.InventoryProduct;
+import com.ims.classes.Manager;
+
+public class ProductsTracker implements Runnable {
+	private Emailer 		emailer;
+	private long 			updateFrequency;
+	private long			lastTimeChecked;
+	private DBUtilities		db = new DBUtilities();
 	
-	public void start() {
-		while(true){
-			System.err.println("Hello Infinite");
-		}
+	/**
+	 * @param updateFrequency The updateFrequency in milliseconds
+	 */
+	public ProductsTracker(long updateFrequency) {
+		this.updateFrequency 	= updateFrequency;
+		emailer 				= new Emailer();
+		lastTimeChecked			= 0;
 	}
 	
-	public void notifyManager() {
+	public long notifyAboutProductsInNeedToOfRestock() {
+		ArrayList<InventoryProduct> products = db.getProductsToBeRestocked();
 		
+		if( products.size() < 1 ) {
+			return -1;
+		}
+		
+		/*
+		 * Notify the manager of each supermarket that
+		 * these products need to be re-stocked
+		 * 
+		 * These products are added to map containing a list of products.
+		 * The map is structured as supermarked_id => list of products
+		 */
+		HashMap<Integer, ArrayList<InventoryProduct> > listToNotify = new HashMap<Integer, ArrayList<InventoryProduct> >();
+		
+		/*
+		 * Arrange products by supermarket so that a single message is sent
+		 * to each supermarket
+		 */
+		for(InventoryProduct product : products ) {
+			if( listToNotify.containsKey(product.getSupermarketID()) ) {
+				listToNotify.get(product.getSupermarketID()).add(product);
+			} else {
+				ArrayList<InventoryProduct> list = new ArrayList<InventoryProduct>();
+				list.add(product);
+				listToNotify.put(product.getSupermarketID(), list);
+			}
+		}
+		
+		notifyManagers(listToNotify);
+		
+		/*
+		 * The time that it finishes running
+		 */
+		return System.currentTimeMillis();
+	}
+	
+	public void notifyManagers(HashMap<Integer, ArrayList<InventoryProduct> > listToNotify) {
+		Set<Integer> supermarketsId = listToNotify.keySet();
+		for(Integer supermarketId : supermarketsId) {
+			Manager manager = db.getManagerBySupermarketId(supermarketId);
+			/*
+			 * Send email to managers in supermarket
+			 */
+			emailer.sendEmail(manager.getEmailAddress(), listToNotify.get(supermarketId));	
+		}
+	}
+
+	@Override
+	public void run() {
+		while(true){
+			try {
+				lastTimeChecked = notifyAboutProductsInNeedToOfRestock();
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
